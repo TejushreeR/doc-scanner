91ea624231abc411d9519aa041c03179546caa36
TejushreeR <134353230+TejushreeR@users.noreply.github.com>
2025-09-09 14:47:30 +0530

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,10 +1,10 @@
-// src/firebase.js
+
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
 
-// Your Firebase configuration
+
 const firebaseConfig = {
   apiKey: "AIzaSyCQ0s1sdhj4m2WbA7pXBLa-HzkaK040wr4",
   authDomain: "doc-scanner-e9cdc.firebaseapp.com",
@@ -15,10 +15,10 @@ const firebaseConfig = {
   measurementId: "G-X2SJ3T3HQ4",
 };
 
-// Initialize Firebase
+
 const app = initializeApp(firebaseConfig);
 
-// Export Firebase services so you can use them in other files
+
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const storage = getStorage(app);
